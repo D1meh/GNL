85b8e9d81d9975042ff69bb1bb2deffdf57db24a
D1meh <80787893+D1meh@users.noreply.github.com>
2021-04-05 18:22:14 +0200

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,122 +6,86 @@
 /*   By: mtaouil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 14:46:55 by mtaouil           #+#    #+#             */
-/*   Updated: 2021/03/30 14:47:05 by mtaouil          ###   ########.fr       */
+/*   Updated: 2021/04/02 11:31:31 by mtaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char    *dest;
-    int     size;
-    int     i;
-    int     j;
+	char	*dest;
+	int		size;
+	int		i;
+	int		j;
 
-    if (!s1)
-        return (s2);
-    if (!s2)
-        return (s1);
-    size = ft_strlen(s1) + ft_strlen(s2);
-    dest = malloc(sizeof(char) * size + 1);
-    if (!dest)
-        return (NULL);
-    i = 0;
-    j = 0;
-    while (s1[i])
-    {
-        dest[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-        dest[i++] = s2[j++];
-    dest[i] = '\0';
-    return (dest);
+	if (!s1)
+		return (s2);
+	if (!s2)
+		return (s1);
+	size = ft_strlen(s1) + ft_strlen(s2);
+	dest = malloc(sizeof(char) * size + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		dest[i++] = s2[j++];
+	dest[i] = '\0';
+	return (dest);
 }
 
-char    *new_line(int fd)
+int	new_line(int fd, char **s)
 {
-    char    *s;
-    char    *tmp;
-    char    buf[BUFFER_SIZE + 1];
-    int     n;
+	int		n;
+	char	*tmp;
+	char	buf[BUFFER_SIZE + 1];
 
-    s = ft_strdup("");
-    n = read(fd, buf, BUFFER_SIZE);
-    while (n != 0)
-    {
-        if (n < 0)
-            return (NULL);
-        buf[n] = '\0';
-        tmp = ft_strjoin(s, buf);
-        free(s);
-        s = tmp;
-        if (ft_strchr(buf, '\n') != NULL)
-            break ;
-        n = read(fd, buf, BUFFER_SIZE);
-    }
-    return (s);
+	n = read(fd, buf, BUFFER_SIZE);
+	if (n < 1)
+		return (n);
+	buf[n] = '\0';
+	tmp = ft_strjoin(*s, buf);
+	free(*s);
+	*s = tmp;
+	return (n);
 }
 
-char    *recup_buff(char *s, int fd)
+void	final_free(void **s)
 {
-    char    *tmp;
-    char    *buf;
-
-    tmp = new_line(fd);
-    buf = ft_strjoin(s, tmp);
-    free(tmp);
-    return (buf);
+	free(*s);
+	*s = NULL;
 }
 
-void    delete_if(void **s, int *a)
+int	get_next_line(int fd, char **line)
 {
-    if (*a > 0)
-    {
-        free(*s);
-        *s = NULL;
-    }
-    *a = *a + 1;
+	static char	*s = NULL;
+	char		*tmp;
+	int			n;
+
+	if (fd < 0 || !line || BUFFER_SIZE < 1 || read(fd, s, 0) < 0)
+		return (-1);
+	if (s == NULL)
+		s = ft_strdup("");
+	n = 1;
+	while (!ft_strchr(s, '\n') && n)
+		n = new_line(fd, &s);
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		*line = ft_strdup(s);
+	else
+		*line = ft_substr(s, 0, bufL(s));
+	tmp = ft_substr(s, bufL(s) + 1, ft_strlen(s) - bufL(s));
+	free(s);
+	s = tmp;
+	if (n > 0)
+		return (1);
+	final_free((void **)&s);
+	return (0);
 }
-
-int get_next_line(int fd, char **line)
-{
-    static char *s = "";
-    char        *buf;
-    int         len;
-    static int  a = 0;
-
-    if (fd < 0 || read(fd, s, 0) < 0 || BUFFER_SIZE < 1 || !line)
-        return (-1);
-    if (ft_strchr(s, '\n') != NULL)
-        buf = s;
-    else
-        buf = recup_buff(s, fd);
-    if (!buf)
-        return (-1);
-    len = buff_len(buf);
-    *line = ft_substr(buf, 0, len);
-    delete_if((void **)&s, &a);
-    s = ft_substr(buf, len + 1, ft_strlen(buf) - len);
-    if (ft_strchr(buf, '\n') != NULL)
-    {
-        free(buf);
-        return (1);
-    }
-    free(buf);
-    //free(s);
-    return (0);
-}
-
-/* int main()
-{
-    int fd;
-    char *line;
-
-    fd = open("alphabet.txt", O_RDONLY);
-    get_next_line(fd, &line);
-    get_next_line(fd, &line);
-    get_next_line(fd, &line);
-    free(line);
-} */
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mtaouil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 14:47:25 by mtaouil           #+#    #+#             */
-/*   Updated: 2021/03/30 14:47:28 by mtaouil          ###   ########.fr       */
+/*   Updated: 2021/04/02 11:22:01 by mtaouil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
-#include <stdio.h>
 
-char    *ft_strchr(char *s, int c);
-unsigned int    ft_strlen(char *s);
-char    *ft_substr(char *s, unsigned int start, size_t len);
-char    *ft_strdup(char *s1);
-int     buff_len(char *s);
-char    *ft_strjoin(char *s1, char *s2);
-int get_next_line(int fd, char **line);
+char			*ft_strchr(char *s, int c);
+unsigned int	ft_strlen(char *s);
+char			*ft_substr(char *s, unsigned int start, size_t len);
+char			*ft_strdup(char *s1);
+int				bufL(char *s);
+char			*ft_strjoin(char *s1, char *s2);
+int				new_line(int fd, char **s);
+void			final_free(void **s);
+int				get_next_line(int fd, char **line);
 
 #endif
